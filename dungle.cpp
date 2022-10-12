712636a7663720fdc5733613788a1127ac461690
andrew <a.h.nguyen475@gmail.com>
2022-10-11 22:51:55 -0400

    Added printing for signed intgers via new function in dungle.cpp TODO: create a dungle printing function. Arithmetic and logic functions will then follow

--- dungle.cpp
+++ dungle.cpp
@@ -2,41 +2,62 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <cstring>
 
-void parseDungleLine(std::string line)
+char parseChar(std::string line)
 {
+	line.erase(0,1);
 	std::string subline;
 
 	uint16_t dungleline = 0x0000;
 	uint16_t mask 	    = 0x0080;
 
-	if(subline.find(" ") != std::string::npos)
+	for(int i = 0; i < 8; i++)
 	{
-		std::cout << std::endl;
-	}
-	else
-	{
-		for(int i = 0; i < 8; i++)
+		subline = line.substr(0,6);
+		if(subline == "Dongle")
 		{
-			subline = line.substr(0,6);
-			//std::cout << subline << std::endl;
-			if(subline == "Dongle")
-			{
-				dungleline = dungleline | (mask>>i);
-			}
-			else if(subline == "Dingle")
-			{
-				dungleline = dungleline | 0x0000;
-			}
-			else
-			{
-				break;
-			}
-			line.erase(0,6);
+			dungleline = dungleline | (mask>>i);
 		}
-		//std::cout << std::hex << dungleline << ", " << char(dungleline);
-		std::cout << char(dungleline);
+		else if(subline == "Dingle")
+		{
+			dungleline = dungleline | 0x0000;
+		}
+		else
+		{
+			break;
+		}
+		line.erase(0,6);
 	}
+	return char(dungleline);
+}
+
+short parseInt(std::string line)
+{
+	line.erase(0,1);
+	std::string subline;
+
+	uint16_t dungleline = 0x0000;
+	uint16_t mask 	    = 0x0080;
+
+	for(int i = 0; i < 8; i++)
+	{
+		subline = line.substr(0,6);
+		if(subline == "Dongle")
+		{
+			dungleline = dungleline | (mask>>i);
+		}
+		else if(subline == "Dingle")
+		{
+			dungleline = dungleline | 0x0000;
+		}
+		else
+		{
+			break;
+		}
+		line.erase(0,6);
+	}
+	return short(dungleline);
 }
 
 int main(int argc, char *argv[])
@@ -48,10 +69,13 @@ int main(int argc, char *argv[])
 	}
 
 	std::vector<std::string> inputCode;
-	//std::vector<uint16_t> dungleCode;
+	std::vector<char> dungChars;
+	std::vector<short> dungInts;
 
 	std::string filename(argv[1]);
 	std::string line;
+
+	std::string newdung;
 
 	std::ifstream file;
 	file.open(filename);
@@ -62,16 +86,24 @@ int main(int argc, char *argv[])
 		{
 			std::getline(file, line);
 			inputCode.push_back(line);
+
+			if(line.find("!") != std::string::npos)
+			{
+				dungChars.push_back(parseChar(line));
+			}
+
+			if(line.find("#") != std::string::npos)
+			{
+				dungInts.push_back(parseInt(line));
+			}
 		}	
 		file.close();
 	}
 
-	for(int i = 0; i < inputCode.size(); i++)
-	{
-		//std::cout << inputCode.at(i) << " | ";
-		parseDungleLine(inputCode.at(i));
-		//std::cout << std::endl;
-	}
+	for(int i = 0; i < dungChars.size(); i++) printf("%c", dungChars.at(i));
 	std::cout << std::endl;
+	for(int i = 0; i < dungInts.size(); i++) printf("%d", dungInts.at(i)); 
+	std::cout << std::endl;
+
 	return 0;
 }
