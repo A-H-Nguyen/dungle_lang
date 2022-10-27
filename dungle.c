d84c620a87d5ddbcd1d3b323d8315e6108799bec
andrew <a.h.nguyen475@gmail.com>
2022-10-26 22:30:27 -0400

    Initial commit for C branch -- will eventually make this the master branch because I like C

---
+++ dungle.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+//#include <stdio.h>
+//
+
+#define LineLen 16
+#define NumRegs 16
+
+int main(int argc, char **argv)
+{
+	short *DungleData = (short*)malloc(LineLen*NumRegs);
+	return 0;
+}
